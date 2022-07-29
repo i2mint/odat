d454cbb8058b49219a574263e84a222b8f4e2695
sylvainbonnot <sppbonnot@gmail.com>
2022-07-29 15:55:57 +0200

    update

--- odat/utils/functional_dacc.py
+++ odat/utils/functional_dacc.py
@@ -1,6 +1,8 @@
 """
 Utils to create daccs from dags
 """
+
+
 from meshed import DAG, code_to_dag
 from typing import Iterable
 
@@ -19,3 +21,15 @@ def is_subset(iterable1: Iterable, iterable2: Iterable):
 
 def validate_input_dacc_gen(input_tuple, intermediate_nodes):
     return is_subset(input_tuple, intermediate_nodes)
+
+#mall of inputs with keys that are the roots of the dag
+Dacc(dag, input_mall)
+# check gurgle
+dacc.gen('chks', 'tags')-> calls dag[:chks]
+
+
+#def tag_wf(wf: WaveForm, tag: Tag):
+    pass
+# check simple.py in examples
+# dans dropdown
+input: WaveForm 
